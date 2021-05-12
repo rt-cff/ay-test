18102f2da14b103874cd067397c6e8b5330c350d
Raymond Tam <raymondtamtwtp@gmail.com>
2021-05-12 22:07:17 +0800

    feat(components): implement NumberPicker & styling

--- src/components/GuestsRoomsPicker.jsx
+++ src/components/GuestsRoomsPicker.jsx
@@ -1,12 +1,14 @@
 import PropTypes from "prop-types";
 import { NumberPicker } from "./";
 
+import "./GuestsRoomsPicker.css";
+
 const GuestNumberPicker = () => {
   return (
-    <div>
-      <div>
-        <div>大人</div>
-        <div>年齡 20+</div>
+    <div styleName="guest-number-picker">
+      <div styleName="room-guest">
+        <div styleName="room-guest-type">大人</div>
+        <div styleName="room-guest-description">年齡 20+</div>
       </div>
       <NumberPicker />
     </div>
@@ -16,7 +18,7 @@ const GuestNumberPicker = () => {
 const Room = () => {
   return (
     <>
-      <div style={{ margin: "16px 0px" }}>房間：2 人</div>
+      <div styleName="room-description">房間：2 人</div>
       <GuestNumberPicker />
     </>
   );
@@ -26,7 +28,7 @@ const GuestRoomPicker = ({ people, rooms, handleDistribution }) => {
   //TODO: implement Number Picker
   return (
     <div style={{ width: 380, height: 380, background: "white", padding: 16 }}>
-      <h4>住客人數：3 人 / 2 房</h4>
+      <div styleName="title">住客人數：3 人 / 2 房</div>
       <Room />
     </div>
   );
--- src/components/NumberPicker.jsx
+++ src/components/NumberPicker.jsx
@@ -1,3 +1,60 @@
+import "./NumberPicker.css";
+
 export const NumberPicker = () => {
-  return <div>NumberPicker</div>;
+  return (
+    <div styleName="number-picker-container">
+      <div styleName="button">
+        <MinusIcon />
+      </div>
+      <div styleName="input-container">
+        <input
+          type="number"
+          name="guest_no"
+          min={0}
+          max={99}
+          step={1}
+          value={5}
+          onClick={() => {}}
+          onBlur={() => {}}
+        />
+      </div>
+      <div styleName="button">
+        <PlusIcon />
+      </div>
+    </div>
+  );
 };
+
+const MinusIcon = () => (
+  <svg
+    width="20"
+    height="2"
+    viewBox="0 0 20 2"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M19.2411 0.357143H0.758924C0.418853 0.357143 0.142853 0.644715 0.142853 1C0.142853 1.35529 0.418853 1.64286 0.758924 1.64286H19.2411C19.5811 1.64286 19.8571 1.35529 19.8571 1C19.8571 0.644715 19.5811 0.357143 19.2411 0.357143Z"
+      fill="#1E9FD2"
+    />
+  </svg>
+);
+
+const PlusIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M19.2411 9.38393H10.6161V0.758928C10.6161 0.418446 10.3401 0.142857 10 0.142857C9.65992 0.142857 9.38392 0.418446 9.38392 0.758928V9.38393H0.758924C0.418853 9.38393 0.142853 9.65952 0.142853 10C0.142853 10.3405 0.418853 10.6161 0.758924 10.6161H9.38392V19.2411C9.38392 19.5816 9.65992 19.8571 10 19.8571C10.3401 19.8571 10.6161 19.5816 10.6161 19.2411V10.6161H19.2411C19.5811 10.6161 19.8571 10.3405 19.8571 10C19.8571 9.65952 19.5811 9.38393 19.2411 9.38393Z"
+      fill="#1E9FD2"
+    />
+  </svg>
+);
