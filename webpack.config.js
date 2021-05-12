673646cd18243a29b8a59d02f05ab50484c4d958
Raymond Tam <raymondtamtwtp@gmail.com>
2021-05-12 14:30:50 +0800

    feat(babel-plugin-react-css-modules): config on babel and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,17 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[path]___[name]__[local]___[hash:base64:5]",
+              },
+            },
+          },
+        ],
       },
     ],
   },
