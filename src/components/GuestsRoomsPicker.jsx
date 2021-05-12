e2ebfb571ba138858eb75632e8a5305e8c6573fc
Raymond Tam <raymondtamtwtp@gmail.com>
2021-05-12 22:18:15 +0800

    feat(components): [GuestRoomsPicker] added typing & styling

--- src/components/GuestsRoomsPicker.jsx
+++ src/components/GuestsRoomsPicker.jsx
@@ -3,12 +3,27 @@ import { NumberPicker } from "./";
 
 import "./GuestsRoomsPicker.css";
 
-const GuestNumberPicker = () => {
+export const GUEST_TYPE = {
+  ADULT: "adult",
+  CHILD: "child",
+};
+
+const GUEST_DESCRIPTION = {
+  [GUEST_TYPE.ADULT]: "大人",
+  [GUEST_TYPE.CHILD]: "小孩",
+};
+
+const GUUEST_AGE = {
+  [GUEST_TYPE.ADULT]: "20",
+  [GUEST_TYPE.CHILD]: "0",
+};
+
+const GuestNumberPicker = ({ type }) => {
   return (
     <div styleName="guest-number-picker">
       <div styleName="room-guest">
-        <div styleName="room-guest-type">大人</div>
-        <div styleName="room-guest-description">年齡 20+</div>
+        <div styleName="room-guest-description">{GUEST_DESCRIPTION[type]}</div>
+        <div styleName="room-guest-age">年齡 {GUUEST_AGE[type]}+</div>
       </div>
       <NumberPicker />
     </div>
@@ -19,7 +34,8 @@ const Room = () => {
   return (
     <>
       <div styleName="room-description">房間：2 人</div>
-      <GuestNumberPicker />
+      <GuestNumberPicker type={GUEST_TYPE.ADULT} />
+      <GuestNumberPicker type={GUEST_TYPE.CHILD} />
     </>
   );
 };
@@ -29,6 +45,8 @@ const GuestRoomPicker = ({ people, rooms, handleDistribution }) => {
   return (
     <div style={{ width: 380, height: 380, background: "white", padding: 16 }}>
       <div styleName="title">住客人數：3 人 / 2 房</div>
+      <Room />
+      <hr class="solid" styleName="divider" />
       <Room />
     </div>
   );
